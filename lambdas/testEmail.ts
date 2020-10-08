36bf3eb696cbf713c402af7ec149591547458d46
SamSteele01 <ssteele017@gmail.com>
2020-10-08 12:33:36 -0400

    debug sending emails

--- lambdas/testEmail.ts
+++ lambdas/testEmail.ts
@@ -1,11 +1,12 @@
-import 'source-map-support/register';
-import fs from 'fs';
-import * as AWS from 'aws-sdk';
-import { Handler, Context } from 'aws-lambda';
-import handlebars from 'handlebars';
+import "source-map-support/register";
+import fs from "fs";
+import * as AWS from "aws-sdk";
+import { Handler, Context, APIGatewayProxyResult } from "aws-lambda";
+import handlebars from "handlebars";
 
 const ses = new AWS.SES();
 
+// TODO: use AWS emailTemplates - need upload script or serverless plugin
 const testHtml = `
   <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "https://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
   <html xmlns="https://www.w3.org/1999/xhtml">
@@ -13,7 +14,7 @@ const testHtml = `
       <!-- The character set should be utf-8 -->
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
       <meta name="viewport" content="width=device-width"/>
-      <!-- Link to the email's CSS, which will be inlined into the email -->
+      <!-- Link to the email"s CSS, which will be inlined into the email -->
       <link rel="stylesheet" href="assets/css/foundation-emails.css">
       <style>
         <!-- Your CSS to inline should be added here -->
@@ -28,10 +29,10 @@ const testHtml = `
           <td class="float-center" align="center" valign="top">
             <center>
               <header>
-                OpenZeppelin Contract Monitor Test Email
+                <h2>OpenZeppelin Contract Monitor Test Email</h2>
               </header>
               <p>
-                {{event}}
+                {{message}}
               </p>
             </center>
           </td>
@@ -44,39 +45,40 @@ const testHtml = `
 /* 
   @req.body { event, email }
 */
-export const send: Handler = async (event, _context) => {
+export const send: Handler = (event, _context, callback) => {
   
-  const data = JSON.parse(event.body)
+  const eventBody = JSON.parse(event.body)
+  console.log("input DATA", eventBody);
   
   // get template
-  // const testHtml = fs.readFileSync('../data/emailTemplates/test.html'); // this doesn't work !!
+  // const testHtml = fs.readFileSync("../data/emailTemplates/test.html"); // this doesn't work !!
   
   // compile
   const template = handlebars.compile(testHtml);
   
-  const email = template({
+  const emailTemplate = template({
     // ...vars
-    event: data.event
+    message: eventBody.message
   })
   
   const params = {
     Source: process.env.FROM_ADDRESS, // has to be verified address
-    ReturnPath: 'it-blew-up@mailinator.com',
+    ReturnPath: "ssteele017@gmail.com",
     Destination: {
       ToAddresses: [
-        data.email
+        eventBody.email
       ]
     },
     Message: {
       Subject: {
-        Data: 'OZ test email'
+        Data: "OZ test email"
       },
       Body: {
         Text: {
-          Data: 'OpenZeppelin Contract Monitor Test Email'
+          Data: "OpenZeppelin Contract Monitor Test Email"
         },
         Html: {
-          Data: email
+          Data: emailTemplate
         }
       }
     }
@@ -84,20 +86,31 @@ export const send: Handler = async (event, _context) => {
   
   // send
   ses.sendEmail(params, (err, data) => {
+    let response: APIGatewayProxyResult = {};
+    
     if (err) {
-      console.log('Error sending test email: ', err);
-      return {
-        statusCode: err.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Error sending test email.',
+      console.log("Error sending test email: ", err);
+      response = {
+        "statusCode": err.statusCode || 501,
+        "headers": { 
+          "Content-Type": "text/plain",
+          "Access-Control-Allow-Origin": "*",
+        },
+        "body": "Error sending test email."
       };
+      callback(null, response);
+      return;
     }
     
-    const response = {
-      statusCode: 200,
-      headers: { 'Content-Type': 'application/json' },
-      body: data,
+    // "Content-Type": "application/json",
+    console.log("DATA", data);
+    response = {
+      "statusCode": 200,
+      "headers": { 
+        "Access-Control-Allow-Origin": "*"
+      },
+      "body": `email successfully sent to ${eventBody.email}`
     };
-    return response;
+    callback(null, response);
   })
 }
